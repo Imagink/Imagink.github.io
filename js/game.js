a15426eef7599e49e2b044afa467bd8ea9e1538d
Ian <ianfchow@me.com>
2016-11-11 14:09:16 +0800

    adding things

--- js/game.js
+++ js/game.js
@@ -170,45 +170,96 @@ $(document).keydown(function(e) {
         break;
 
         default: return; // exit this handler for other keys
+        $('#leona').stop()
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+(document).keyup(function(e) {
+    var Speed = 50;
+    var Drift = 50;
+    switch(e.which) {
+      case 37: // left
+        $(".leona").css("background","url('https://imagink.github.io/img/front.png')");
+        $(".leona").css("background-size","110px 285px");
+        //$(".leona").css("background-position","0px 193px");
+        //$(".leona").css("background-repeat","no-repeat");
+        $('#leona').animate({
+        'marginLeft' : '-='+Speed+"px" //moves right
+        }, 'fast', 'linear');
+      break;
+
+      case 38: // up
+        $(".leona").css("background","url('https://imagink.github.io/img/front.png')");
+        $(".leona").css("background-size","110px 285px");
+        //$(".leona").css("background-position","0px 193px");
+        //$(".leona").css("background-repeat","no-repeat");
+        $('#leona').animate({
+        'marginTop' : '-='+Drift+"px"
+        }, 'fast', 'linear'); 
+      break;
+
+        case 39: // right
+        $(".leona").css("background", "url('https://imagink.github.io/img/front.png')");
+        $(".leona").css("background-size","110px 285px");
+        //$(".leona").css("background-position","0px 193px");
+        //$(".leona").css("background-repeat","no-repeat");
+        $('#leona').animate({
+        'marginLeft' : '+='+Speed+"px" //moves right
+        }, 'fast', 'linear');
+        break;
+
+        case 40: // down
+        $(".leona").css("background","url('https://imagink.github.io/img/front.png')");
+        $(".leona").css("background-size","110px 285px");
+        //$(".leona").css("background-position","0px 193px");
+        //$(".leona").css("background-repeat","no-repeat");
+        $('#leona').animate({
+        'marginTop' : '+='+Drift+"px"
+        }, 'fast', 'linear');
+        break;
+
+        default: return; // exit this handler for other keys
+        $('#leona').stop()
     }
     e.preventDefault(); // prevent the default action (scroll / move caret)
 });
 
  //setTimeout(function(){ $("#leona").css("background","url('https://imagink.github.io/img/front.png')"); }, 1000)
 
-     function collision($div1, $div2) {
-      var x1 = $div1.offset().left;
-      var y1 = $div1.offset().top;
-      var h1 = $div1.outerHeight(true);
-      var w1 = $div1.outerWidth(true);
-      var b1 = y1 + h1;
-      var r1 = x1 + w1;
-      var x2 = $div2.offset().left;
-      var y2 = $div2.offset().top;
-      var h2 = $div2.outerHeight(true);
-      var w2 = $div2.outerWidth(true);
-      var b2 = y2 + h2;
-      var r2 = x2 + w2;
+     //function collision($div1, $div2) {
+      //var x1 = $div1.offset().left;
+      //var y1 = $div1.offset().top;
+      //var h1 = $div1.outerHeight(true);
+      //var w1 = $div1.outerWidth(true);
+      //var b1 = y1 + h1;
+     // var r1 = x1 + w1;
+      //var x2 = $div2.offset().left;
+      //var y2 = $div2.offset().top;
+      //var h2 = $div2.outerHeight(true);
+      //var w2 = $div2.outerWidth(true);
+      //var b2 = y2 + h2;
+      //var r2 = x2 + w2;
         
-      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+      //if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
         //if it is not colliding, make div2 background green
-        $("#book").css('background','green');
-        return false;
-      } 
-      else {  
+        //$("#book").css('background','green');
+        //return false;
+      //} 
+      //else {  
         //if it is colliding, make div2 background yellow
-        $("#book").css('background','yellow');
-        return true;
-      }
-    }
+        //$("#book").css('background','yellow');
+        //return true;
+      //}
+    //}
 
-window.setInterval(function() {
-    $('#result').text(collision($('#leona'), $('#book')));
-}, 200);
+//window.setInterval(function() {
+   // $('#result').text(collision($('#leona'), $('#book')));
+//}, 200);
 
-function randomIntFromInterval(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
+//function randomIntFromInterval(min,max)
+//{
+    //return Math.floor(Math.random()*(max-min+1)+min);
+//}
 
 });
